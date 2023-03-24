fe480338a96cf96940b3e7f8548ea59b5aa29429
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2023-03-25 00:48:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,13 +28,13 @@ cat << EOF >> $PREFIX/etc/proot-distro/kali.sh
 DISTRO_NAME="Kali Linux (nethunter)"
 
 TARBALL_URL['aarch64']="https://kali.download/nethunter-images/current/rootfs/kalifs-arm64-minimal.tar.xz"
-TARBALL_SHA256['aarch64']="149e56700374920a25016e228b93d0ce2aa9eccfe76a06436e48697dc70dd342"
+TARBALL_SHA256['aarch64']="7e17a35e1528a5efc12bf1bbad00a764d38a5724e2b08a226849c594a3b3f029"
 TARBALL_URL['arm']="https://kali.download/nethunter-images/current/rootfs/kalifs-armhf-minimal.tar.xz"
-TARBALL_SHA256['arm']="e9117e6464a98e76e19793d44b6f84c836b1781ba6e01125f5292708be9232a0"
+TARBALL_SHA256['arm']="62f07cd260cd31e9a84c25a331f3db0278d9ccdeb648522b69382946acdd8581"
 TARBALL_URL['i686']="https://kali.download/nethunter-images/current/rootfs/kalifs-i386-minimal.tar.xz"
-TARBALL_SHA256['i686']="6ec934ac10e100b84b5f635afa55a869fdc0c537a5b27e448650415c03b65c1a"
+TARBALL_SHA256['i686']="e83cd8f57d6128efd64e88b191a1653ff315fffd78c05d536d2b6f63b2e6d49d"
 TARBALL_URL['x86_64']="https://kali.download/nethunter-images/current/rootfs/kalifs-amd64-minimal.tar.xz"
-TARBALL_SHA256['x86_64']="e6b239d709f6e39d468404961a2c241a69ac866ebe49b5dc8cdec4a2be585912"
+TARBALL_SHA256['x86_64']="096290b7229ab81f1ac3b35324a7109dc19f1e2f5bf6aab1ff8254ebc95463ea"
 
 EOF
 
